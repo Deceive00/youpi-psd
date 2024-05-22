bc636620e8461518be1daaa18952d726bf8f3616
Deceive00 <ryanliem231204@gmail.com>
2024-05-22 14:08:26 +0700

    fix

--- src/pages/auth/auth-page.tsx
+++ src/pages/auth/auth-page.tsx
@@ -10,7 +10,7 @@ export default function AuthPage() {
           <div className="grid gap-2 text-center">
             <h1 className="text-3xl font-bold">Welcome Back!</h1>
             <p className="text-balance text-muted-foreground">
-              Enter your email below to login to your account
+              Continue with google or 
             </p>
           </div>
           <div className="grid gap-4">
