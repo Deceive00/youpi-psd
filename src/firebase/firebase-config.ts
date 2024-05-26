3fe8080277b351c2b448e384eabe6d5bb55310a1
blogstevens@gmail.com <blogstevens@gmail.com>
2024-05-26 11:47:56 +0700

    youpibro

--- src/firebase/firebase-config.ts
+++ src/firebase/firebase-config.ts
@@ -9,12 +9,12 @@ import { getAuth } from "firebase/auth";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: "youpi-92b43.firebaseapp.com",
-  projectId: "youpi-92b43",
-  storageBucket: "youpi-92b43.appspot.com",
-  messagingSenderId: "313366489691",
-  appId: "1:313366489691:web:3a3c2f99c41b407da02d7e",
-  measurementId: "G-QY4W1NYYZY"
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
--- vite.config.ts
+++ vite.config.ts
@@ -10,15 +10,28 @@ export default defineConfig(({ mode }) => {
   return {
     define: {
       "process.env.REACT_APP_API_KEY": JSON.stringify(env.REACT_APP_API_KEY),
+      "process.env.REACT_APP_AUTH_DOMAIN": JSON.stringify(
+        env.REACT_APP_AUTH_DOMAIN
+      ),
+      "process.env.REACT_APP_PROJECT_ID": JSON.stringify(
+        env.REACT_APP_PROJECT_ID
+      ),
+      "process.env.REACT_APP_STORAGE_BUCKET": JSON.stringify(
+        env.REACT_APP_STORAGE_BUCKET
+      ),
+      "process.env.REACT_APP_MESSAGING_SENDER_ID": JSON.stringify(
+        env.REACT_APP_MESSAGING_SENDER_ID
+      ),
+      "process.env.REACT_APP_APP_ID": JSON.stringify(env.REACT_APP_APP_ID),
+      "process.env.REACT_APP_MEASUREMENT_ID": JSON.stringify(
+        env.REACT_APP_MEASUREMENT_ID
+      ),
     },
-    plugins: [
-      react(),
-      tsconfigPaths()
-    ],
+    plugins: [react(), tsconfigPaths()],
     css: {
       modules: {
-        localsConvention: "camelCase"
-      }
+        localsConvention: "camelCase",
+      },
     },
     resolve: {
       alias: {
