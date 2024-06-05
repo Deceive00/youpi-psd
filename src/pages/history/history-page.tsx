bfdb0858416fde82866ecd33fe7c026599ca19df
Yoseftian Nuhedward <yoseftiannuhedward@gmail.com>
2024-06-05 23:08:01 +0700

    feed: new history page masih pakai dummy

--- src/pages/history/history-page.tsx
+++ src/pages/history/history-page.tsx
@@ -1,11 +1,68 @@
-import MainLayout from "src/layout/main-layout"
+import MainLayout from "src/layout/main-layout";
+import OrderCard from "./finished-order-card";
 
 const HistoryPage = () => {
- return (
-    <MainLayout className={`pt-14 sm:pt-16 overflow-x-hidden`}>
-        <div className={`w-screen h-max font-nunito box-border px-16`}>History Page</div>
-    </MainLayout>
- )
-}
+  // Fetch from firebase 'orders'
 
-export default HistoryPage
+  // Use Effect 
+
+  // Set Image berdasarkan Jenis Category Transaksinya, ambil dari static image krn cuman 2 gambar aja
+    // Pick Up = 
+    // Delivery = 
+
+  // Reorder Handler
+
+  // View Detail Handler
+
+  // Calculate Total Price for each Transaction
+
+  return (
+    <MainLayout className={`py-16 overflow-x-hidden`}>
+      <div className={`w-screen h-max font-nunito box-border mx-auto flex justify-center`}>
+        <div className={`grid lg:grid-cols-3 lg:gap-12`}>
+        <OrderCard
+            imageUrl="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            title="Shoes"
+            campusName="Binus Alam Sutera"
+          />
+
+          {/* 2 */}
+          <OrderCard
+            imageUrl="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            title="Ramen"
+            campusName="Binus Alam Sutera"
+          />
+
+          {/* 3 */}
+          <OrderCard
+            imageUrl="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            title="Shoes"
+            campusName="Binus Alam Sutera"
+          />
+
+          <OrderCard
+            imageUrl="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            title="Shoes"
+            campusName="Binus Alam Sutera"
+          />
+
+          {/* 2 */}
+          <OrderCard
+            imageUrl="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            title="Shoes"
+            campusName="Binus Alam Sutera"
+          />
+
+          {/* 3 */}
+          <OrderCard
+            imageUrl="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            title="Shoes"
+            campusName="Binus Alam Sutera"
+          />
+        </div>
+      </div>
+    </MainLayout>
+  );
+};
+
+export default HistoryPage;
